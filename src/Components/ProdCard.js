ead1dbecdbebc6a169191c0ec66af6e995c7194c
Madhurima-Ghosh18 <140677876+Madhurima-Ghosh18@users.noreply.github.com>
2024-08-11 03:41:11 +0530

    Update ProdCard.js

--- src/Components/ProdCard.js
+++ src/Components/ProdCard.js
@@ -5,19 +5,20 @@ const ProdCard = ({ header, description, link, img }) => {
         <div className="prod-card d-flex px-3 py-3 justify-content-between">
             <div className="text-container d-flex flex-column justify-content-around flex-grow-1" style={{
                 width: "59%",
+                maxWidth: "50vw !important", 
             }}>
                 <div>
                     <i style={{ fontSize: "1.5vw" }} className="fa-solid fa-grip-vertical"></i>
                     <h2 className="text-start d-inline" style={{
                         fontSize: "1.5vw", marginLeft: "15px", color: "var(--primary-color) !important"
                     }}>{header}</h2>
-                <p className="text-start" style={{ fontSize: "1.1vw", opacity : "0.75" }}>
-                    {description}
-                </p>
+                    <p className="text-start" style={{ fontSize: "1.1vw", opacity: "0.75" }}>
+                        {description}
+                    </p>
                 </div>
                 <a href={link}>
                     <Button variant="dark rounded" style={{
-                        fontSize : "1.1vw",
+                        fontSize: "1.1vw",
                     }}>
                         View Details <i className="fa-solid fa-list"></i>
                     </Button>
