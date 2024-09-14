e1b1d649b3d1859d5a384425ae7dee5503eb3a54
UserDebs <roumya2020second@gmail.com>
2024-09-15 00:14:45 +0530

    Bug fixes

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import Section from "../Components/Section";
 import { Row, Col, Button, Form, Container } from 'react-bootstrap';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [subject, setSubject] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [hide, setHide] = useState(true);
+
+  useEffect(() => {
+    setHide(!(name.length && email.length && subject.length && message.length));
+  }, [name, email, subject, message])
+
+  const handleClick = (e) => {
+      e.preventDefault(); 
+      if(hide) return;
+      window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent('mail@cognitotechnologies.com')}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(`I am ${name}.\nMy email id is ${email}\n${message}`)}`, '_blank'); 
+      setName('');
+      setEmail('');
+      setSubject('');
+      setMessage('');
+  }
   return (
     <Section ID="contact" header="Contact Us">
       <div className="d-flex w-100 justify-content-center align-items-center">
@@ -15,25 +35,28 @@ const Contact = () => {
                   <Col>
                     <Form.Group className="mb-3" controlId="formName">
                       <Form.Label>Enter your Name</Form.Label>
-                      <Form.Control type="text" placeholder="Name" />
+                      <Form.Control required type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
                     </Form.Group>
                   </Col>
                   <Col>
                     <Form.Group className="mb-3" controlId="formSubject">
                       <Form.Label>Subject</Form.Label>
-                      <Form.Control type="text" placeholder="Subject" />
+                      <Form.Control required type="text" placeholder="Subject" value={subject} onChange={(e) => setSubject(e.target.value)}/>
                     </Form.Group>
                   </Col>
                 </Row>
                 <Form.Group className="mb-3" controlId="formEmail">
                   <Form.Label>Enter your Email</Form.Label>
-                  <Form.Control type="email" placeholder="example@example.com" />
+                  <Form.Control required type="email" placeholder="example@example.com" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formMessage">
                   <Form.Label>Enter the Message</Form.Label>
-                  <Form.Control as="textarea" rows={3} placeholder="message..." />
+                  <Form.Control required as="textarea" rows={3} placeholder="message..." value={message} onChange={(e) => setMessage(e.target.value)}/>
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button onClick={handleClick} style={{
+                  opacity : `${hide? '0.5' : '1'}`,
+                  cursor : `${hide? 'not-allowed' : 'pointer'}`
+                }} variant="dark" type="submit">
                   Submit
                 </Button>
               </Form>
